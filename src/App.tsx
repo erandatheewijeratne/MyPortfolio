262006dc6ed399ce8b53f0b8c365b4514656c234
Erandathee Wijeratne <77802957+erandatheewijeratne@users.noreply.github.com>
2025-11-22 08:58:42 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-3xl underline">Welcome to my 3D portfolio!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
